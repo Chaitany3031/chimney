c21eda3955d2c8e82d170e002f24d2c1bc856ad2
surajsibi <surajsibi2022@gmail.com>
2025-06-04 19:28:25 +0530

    final commit

--- script.js
+++ script.js
@@ -31,21 +31,43 @@ hamburger.addEventListener('click', () => {
 
 
 
-  const { animate } = window.motion;
-  const slider = document.getElementById("slider");
-  const cardWidth = slider.children[0].offsetWidth + 16; // width + gap
-  let index = 0;
+  // const { animate } = window.motion;
+  // const slider = document.getElementById("slider");
+  // const cardWidth = slider.children[0].offsetWidth + 16; // width + gap
+  // let index = 0;
 
-  setInterval(() => {
-    index++;
-    if (index * cardWidth >= slider.scrollWidth - slider.clientWidth) {
-      index = 0;
+  // setInterval(() => {
+  //   index++;
+  //   if (index * cardWidth >= slider.scrollWidth - slider.clientWidth) {
+  //     index = 0;
+  //   }
+
+  //   animate(
+  //     slider,
+  //     { transform: `translateX(-${index * cardWidth}px)` },
+  //     { duration: 0.5, easing: "ease-in-out" }
+  //   );
+  // }, 2000);
+
+
+
+  function showTab(tabId, btnElement) {
+      // Hide all tab contents
+      const contents = document.querySelectorAll('.tab-content');
+      console.log(contents)
+      contents.forEach(content => content.classList.add('hidden'));
+
+      // Remove yellow style from all buttons
+      const buttons = document.querySelectorAll('.tab-btn');
+      buttons.forEach(btn => {
+        btn.classList.remove('bg-yellow-400', 'text-black');
+        btn.classList.add('bg-gray-700', 'text-white');
+      });
+
+      // Show selected tab
+      document.getElementById(tabId).classList.remove('hidden');
+
+      // Highlight active button
+      btnElement.classList.add('bg-yellow-400', 'text-black');
+      btnElement.classList.remove('bg-gray-700', 'text-white');
     }
-
-    animate(
-      slider,
-      { transform: `translateX(-${index * cardWidth}px)` },
-      { duration: 0.5, easing: "ease-in-out" }
-    );
-  }, 2000);
-
